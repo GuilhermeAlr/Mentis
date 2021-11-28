9e09e71103c6d11f05a838de432eca4212fa32eb
Klebaum <guilhermealmendro@gmail.com>
2021-11-28 11:02:31 -0300

    Add video-call

--- includes/db_inc.php
+++ includes/db_inc.php
@@ -1,13 +1,13 @@
 <?php
 
-$serverName = "localhost";
-$dBUsername = "root";
-$dBPassword = "";
-$dBName = "mentis";
+$serverName = getenv("DB_SERVER") ?: "127.0.0.1";
+$dBUsername = getenv("DB_USERNAME") ?: "root";
+$dBPassword = getenv("DB_PASS") ?: "";
+$dBName = getenv("DB_NAME") ?: "mentis";
 
 $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
 
 if (!$conn){
-    die("Conexão falhou: " . mysqli_conncet_error());
+    die("Conexão falhou: " . mysqli_connect_error());
 }
 ?>
