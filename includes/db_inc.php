7e0297e5b55241354654164fb1a577577b3d9673
GuilhermeAlr <guilhermealmendro@gmail.com>
2021-11-28 20:31:20 -0300

    teste-estranho

--- includes/db_inc.php
+++ includes/db_inc.php
@@ -1,9 +1,9 @@
 <?php
 
-$serverName = getenv("DB_SERVER") ?: "127.0.0.1";
-$dBUsername = getenv("DB_USERNAME") ?: "root";
-$dBPassword = getenv("DB_PASS") ?: "";
-$dBName = getenv("DB_NAME") ?: "mentis";
+$serverName = "localhost";
+$dBUsername = "root";
+$dBPassword = "";
+$dBName = "mentis";
 
 $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
 
